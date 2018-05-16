d0b62152b9a8d2a7494affe285079543149a37a4
Christiana Koudigkeli <christiana.koudigkeli@nokia.com>
2018-05-16 12:34:08 +0300

    db creation

--- db.sql
+++ db.sql
@@ -7,3 +7,8 @@ INSERT INTO users (username, userpass) VALUES ('user1', 'pass1');
 
 INSERT INTO users (username, userpass) VALUES ('user2', 'pass2');
 
+CREATE TABLE poll (id INT(10) unsigned NOT NULL AUTO_INCREMENT
+PRIMARY KEY, question VARCHAR(250) NOT NULL);
+
+CREATE TABLE participation (id INT(10) unsigned NOT NULL AUTO_INCREMENT
+PRIMARY KEY, username VARCHAR(250) NOT NULL, voted varchar(5));
